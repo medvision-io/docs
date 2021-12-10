8e805b94f17f6c55d43bdcb6c8df0a46990da831
burnpiro <kemalpiro@gmail.com>
2021-12-10 14:19:50 +0100

    Fix react-json-view dependency on Document when building static website

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,3 +3,18 @@ const createPages = require('./gatsby/create-pages')
 
 exports.createPages = createPages
 exports.onCreateNode = onCreateNode
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html" || stage === "develop-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /react-json-view/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
