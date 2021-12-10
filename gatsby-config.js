ecbe68af84179fa371672ee1d5f7c82f65dd06d6
burnpiro <kemalpiro@gmail.com>
2021-12-11 00:24:37 +0100

    Export to config external file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 const packageJSON = require("./package.json");
+const siteConfig = require("./siteConfig");
 
 module.exports = {
   plugins: [
@@ -84,75 +85,5 @@ module.exports = {
       },
     },
   ],
-  siteMetadata: {
-    title: "zhiva.ai",
-    siteUrl: 'https://docs.zhiva.ai',
-    description: "Documentation for zhiva.ai",
-    sections: [
-      {
-        name: 'Build',
-        key: 'build'
-      },
-      {
-        name: 'Quality',
-        key: 'quality'
-      }
-    ],
-    categories: [
-      {
-        name: 'Authentication',
-        key: 'authentication',
-        icon: 'users',
-        section: 'build'
-      },
-      {
-        name: 'Database',
-        key: 'database',
-        icon: 'database',
-        section: 'build'
-      },
-      {
-        name: 'Storage',
-        key: 'storage',
-        icon: 'image',
-        section: 'build'
-      },
-      {
-        name: 'Hosting',
-        key: 'hosting',
-        icon: 'world',
-        section: 'build'
-      },
-      {
-        name: 'Functions',
-        key: 'functions',
-        icon: 'width',
-        section: 'build'
-      },
-      {
-        name: 'Machine Learning',
-        key: 'ml',
-        icon: 'connectors',
-        section: 'build'
-      },
-      {
-        name: 'Analytics',
-        key: 'analytics',
-        icon: 'setup',
-        section: 'quality'
-      },
-      {
-        name: 'Performance',
-        key: 'performance',
-        icon: 'stopwatch',
-        section: 'quality'
-      },
-      {
-        name: 'Test Lab',
-        key: 'test-lab',
-        icon: 'phone_setup',
-        section: 'quality'
-      }
-    ]
-  },
+  siteMetadata: siteConfig,
 };
---
+++ siteConfig.js
@@ -0,0 +1,71 @@
+module.exports = {
+  title: "zhiva.ai",
+  siteUrl: 'https://docs.zhiva.ai',
+  description: "Documentation for zhiva.ai",
+  sections: [
+    {
+      name: 'Build',
+      key: 'build'
+    },
+    {
+      name: 'Quality',
+      key: 'quality'
+    }
+  ],
+  categories: [
+    {
+      name: 'Authentication',
+      key: 'authentication',
+      icon: 'users',
+      section: 'build'
+    },
+    {
+      name: 'Database',
+      key: 'database',
+      icon: 'database',
+      section: 'build'
+    },
+    {
+      name: 'Storage',
+      key: 'storage',
+      icon: 'image',
+      section: 'build'
+    },
+    {
+      name: 'Hosting',
+      key: 'hosting',
+      icon: 'world',
+      section: 'build'
+    },
+    {
+      name: 'Functions',
+      key: 'functions',
+      icon: 'width',
+      section: 'build'
+    },
+    {
+      name: 'Machine Learning',
+      key: 'ml',
+      icon: 'connectors',
+      section: 'build'
+    },
+    {
+      name: 'Analytics',
+      key: 'analytics',
+      icon: 'setup',
+      section: 'quality'
+    },
+    {
+      name: 'Performance',
+      key: 'performance',
+      icon: 'stopwatch',
+      section: 'quality'
+    },
+    {
+      name: 'Test Lab',
+      key: 'test-lab',
+      icon: 'phone_setup',
+      section: 'quality'
+    }
+  ]
+}
