5768028ec25451071a621d41c2f61918748afe5c
burnpiro <kemalpiro@gmail.com>
2021-12-12 23:05:56 +0100

    Add slug base on title and example of an image

---
+++ content/pages/testpage/testpage.md
@@ -0,0 +1,19 @@
+---
+title: "My page!"
+date: "2017-09-18T23:19:51.246Z"
+docVersion: x
+description: Just a test page, nothing to see here
+category: authentication
+isTop: true
+---
+
+Some content dat
+![Desc](./x-group-structure.png)
+
+$test x^2$
+
+$$
+test
+x^2
+$$
+
--- gatsby/on-create-node.js
+++ gatsby/on-create-node.js
@@ -1,9 +1,17 @@
 const { createFilePath } = require(`gatsby-source-filesystem`);
+const kebabCase = require("lodash.kebabcase");
 
 function onCreateNode({ node, getNode, actions }) {
   const { createNodeField } = actions;
   if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` });
+    let slug = "";
+    if (node.frontmatter.slug && node.frontmatter.slug.length > 0) {
+      slug = `/${node.frontmatter.slug}`;
+    } else if (node.frontmatter.title && node.frontmatter.title.length > 0) {
+      slug = `/${kebabCase(node.frontmatter.title.replace(/\W/g, ""))}`;
+    } else {
+      slug = createFilePath({ node, getNode, basePath: `pages` });
+    }
     createNodeField({
       node,
       name: `slug`,
