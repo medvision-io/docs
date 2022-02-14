49223f14dd5fd33f02b452db713c42dfa1b12cbc
burnpiro <kemalpiro@gmail.com>
2022-02-15 00:10:16 +0100

    Hide AppBar when scrolling down

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -11,6 +11,8 @@ import Tabs from "@mui/material/Tabs";
 import Toolbar from "@mui/material/Toolbar";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import Slide from "@mui/material/Slide";
 
 const lightColor = "rgba(255, 255, 255, 0.7)";
 
@@ -24,6 +26,22 @@ interface HeaderProps {
   }[];
 }
 
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
 export default function Header({
   onDrawerToggle,
   title,
@@ -31,40 +49,42 @@ export default function Header({
 }: HeaderProps) {
   return (
     <React.Fragment>
-      <AppBar color="primary" position="sticky" elevation={0}>
-        <Toolbar>
-          <Grid container spacing={1} alignItems="center">
-            <Grid sx={{ display: { sm: "none", xs: "block" } }} item>
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={onDrawerToggle}
-                edge="start"
-              >
-                <MenuIcon />
-              </IconButton>
+      <HideOnScroll>
+        <AppBar color="primary" position="sticky" elevation={0}>
+          <Toolbar>
+            <Grid container spacing={1} alignItems="center">
+              <Grid sx={{ display: { sm: "none", xs: "block" } }} item>
+                <IconButton
+                  color="inherit"
+                  aria-label="open drawer"
+                  onClick={onDrawerToggle}
+                  edge="start"
+                >
+                  <MenuIcon />
+                </IconButton>
+              </Grid>
+              <Grid item xs />
+              <Grid item>
+                <Link
+                  href="https://zhiva.ai"
+                  variant="body2"
+                  sx={{
+                    textDecoration: "none",
+                    color: lightColor,
+                    "&:hover": {
+                      color: "common.white",
+                    },
+                  }}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Go to <strong>zhiva.ai</strong> homepage
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item xs />
-            <Grid item>
-              <Link
-                href="https://zhiva.ai"
-                variant="body2"
-                sx={{
-                  textDecoration: "none",
-                  color: lightColor,
-                  "&:hover": {
-                    color: "common.white",
-                  },
-                }}
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                Go to <strong>zhiva.ai</strong> homepage
-              </Link>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
       {title && (
         <AppBar
           component="div"
